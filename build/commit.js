13752a32c575f0459a8febb88d048f2fa70f2c21
Cedric van Putten <me@bycedric.com>
2019-09-11 12:53:21 +0000

    test some stuff

---
+++ build/commit.js
@@ -0,0 +1,12 @@
+"use strict";
+// import * as core from '@actions/core';
+// import * as github from '@actions/github';
+// /**
+//  * Determine the range of commits to lint.
+//  * It reads the `from` and `to` inputs to determine this.
+//  */
+// export function range() {
+// 	return {
+// 		from: core.getInput('')
+// 	};
+// }
